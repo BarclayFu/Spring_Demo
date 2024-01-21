426a900888038fa44cb8e5711ae5bee0f4886f61
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2024-01-21 18:09:50 -0500

    06 demo

--- demo/src/main/java/com/barclay/demo/AppGamingBasic.java
+++ demo/src/main/java/com/barclay/demo/AppGamingBasic.java
@@ -2,12 +2,16 @@ package com.barclay.demo;
 
 import com.barclay.demo.game.GameRunner;
 import com.barclay.demo.game.MarioGame;
+import com.barclay.demo.game.PacmanGame;
+import com.barclay.demo.game.SuperContraGame;
 
 public class AppGamingBasic {
 
 	public static void main(String[] args) {
-		var marioGame = new MarioGame();
-		var gameRunner = new GameRunner(marioGame);
+		//var marioGame = new MarioGame();
+//		var game = new SuperContraGame();
+		var game = new PacmanGame();
+		var gameRunner = new GameRunner(game);
 		
 		gameRunner.run();
 
